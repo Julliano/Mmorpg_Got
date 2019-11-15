7636419d900678c6bf7133a9e4b9ca0eed1bf78a
Julliano Volpato <jullianovosorio@gmail.com>
2019-11-15 22:42:11 +0100

    finished load user skills

--- app/controllers/jogo.js
+++ app/controllers/jogo.js
@@ -1,8 +1,21 @@
 module.exports.jogo = function(application, req, res){
-	if(req.session.autorizado) {
-		res.render('jogo', {img_casa: req.session.casa});
-	} else {
+	if(!req.session.autorizado) {
 		res.send('Usuário precisa se logar antes.')
+		return;
 	}
+
+	const usuario = req.session.usuario;
+	const casa = req.session.casa;
+
+	const connection = application.config.dbConnection;
+	const JogoDAO = new application.app.models.JogoDAO(connection);
+
+	JogoDAO.iniciaJogo(res, usuario, casa);
 	
+}
+
+module.exports.sair = function(application, req, res) {
+	req.session.destroy(function(err) {
+		res.sender('index', {validacao: {}});
+	});
 }
